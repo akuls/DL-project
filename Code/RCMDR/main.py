233b473e942c21e1c50f176d10056933949d2cea
akuls <akul911@gmail.com>
2017-12-05 19:04:30 -0500

    Added test code complete. Need to batch

--- Code/RCMDR/main.py
+++ Code/RCMDR/main.py
@@ -8,55 +8,93 @@ import rcmdr_model as rcmdr_model
 if HAVE_CUDA:
 	import torch.cuda as cuda
 
-def compute_metrics(pred, triples, topN=10):
+def compute_PRF_HR(topN_list, ground_truth_dict, topK):
 
-	final_triple = [] #This will store (user_idx, pred_score, item_idx)
-	ground_truth_dict = {} #This will map (user_idx, item_idx) to (ground_truth)
+	test_dict = get_dict_from_index_mapping("../../Data/user_item_test.txt")
+	n = len(topK_list)
+	prev_user = -1
+	tp = 0
+	fp = 0
+	start = 0
+	user_precision = []
+	user_recall = []
+	user_AP = []
+	temp_ap = 0.0
+	MAP = 0.0
+	num_users = 0
+	total_hits = 0
+
+	for i in range(n):
+		u, val, v = topK_list[i]
+
+		if u != prev_user:
+			if(prev_user != -1):
+				print 'HR@', topK, 'for user', prev_user, '= ', float(tp)/float(topK)
+				user_AP.append((prev_user, temp_ap))
+				user_precision.append((prev_user, float(tp)/(float(tp)+float(fp))))
+				user_recall.append((prev_user, float(fp)/float(len(test_dict[prev_user]))))
+				MAP += temp_ap
+				num_users += 1
+				total_hits += tp
+
+			prev_user = u
+			tp = fp = 0
+			start = i
+			temp_ap = 0.0
+
+		if(ground_truth_dict[(u, v)] == 1):
+			tp += 1
+		else:
+			fp += 1
+
+		#Running ratio of tp/total_seen_till_now for each user
+		temp_ap += float(tp)/float(i-start+1)
+
+	#Last user outside the loop	
+	print 'HR@', topK, 'for user', prev_user, '= ', float(tp)/float(topK)
+	user_AP.append((prev_user, temp_ap))
+	user_precision.append((prev_user, float(tp)/(float(tp)+float(fp))))
+	user_recall.append((prev_user, float(fp)/float(len(test_dict[prev_user]))))
+	MAP += temp_ap
+	num_users += 1
+
+	MAP /= float(num_users)
+	print 'MAP is', MAP
+
+	print 'Average HR@', topK, 'per user is', total_hits/float(num_users)
+
+
+def compute_metrics(pred, triples, topK=10):
+
+	final_triple = [] #This will have (user_idx, pred_score, item_idx)
+	ground_truth_dict = {} #This will map (user_idx, item_idx) to (ground_truth of 1 or 0)
 
 	n = len(triples)
 	for i in range(n):
-		u, v, ground_truth_val = triple[i]
+		u, v, ground_truth_dict[(u, v)] = triples[i]
 		final_triple.append((u, pred.data[i], v))
-		ground_truth_dict[(u, v)] = ground_truth_val
-
-	#Sort them 	
+		
+	#Sort them while keeping user's data together
 	sorted_triple = sorted(final_triple, reverse=True)
 
-	#Iterate this and for every new user, pick the first 10 items
-	topN_list = []
+	#Iterate this and for every new user, pick the first topK items
+	topK_list = []
 	prev_user = -1
 	i = 0
 	while(i<n):
 		u, _, _ = sorted_triple[i]
 		if(u != prev_user):
+			print 'New user at position', i 
 			prev_user = u
-			topN_list.extend(sorted_triple[i:i+topN])
-			i += 10
+			topK_list.extend(sorted_triple[i:i+topK])
+			i += topK
 
 		else:
 			i += 1
 
-	#At this point, topN has topN for all users sorted in descending order
+	#At this point, topN_list has topK for all users sorted in descending order
 	#Use this and the ground truth dictionary to compute metrics for each/all users
-	n = len(topN)
-	prev_user = -1
-	total_hits = 0
-	hits = 0
-	for i in range(n):
-		u, val, v = topN[i]
-
-		if u != prev_user:
-			if prev_user != -1:
-				print 'HR@', topN, 'for user', prev_user, '= ', hits/float(topN)
-				total_hits += hits
-			prev_user = u
-			hits = 0
-
-		if(ground_truth_dict[(u, v)] == 1 and round(val) == 1):
-			hits += 1
-
-	print 'Average HR@', topN, 'per user is', total_hits*topN/float(n)
-	pass
+	compute_PRF_HR(topK_list, ground_truth_dict, topK)
 
 def get_data_for_rcmdr(ae_item_vecs, index_triples):
 	
@@ -167,6 +205,7 @@ def run_network(rec_net, AE, item_vecs, batch_size, mode, num_negative, num_epoc
 			##############################END OF TRAIN###################################
 
 		elif mode == 'test':
+			print 'In test block'
 			#Read train dict for negative sampling
 			train_dict = get_dict_from_index_mapping("../../Data/user_item_train.txt")
 
@@ -175,16 +214,21 @@ def run_network(rec_net, AE, item_vecs, batch_size, mode, num_negative, num_epoc
 			for user_idx, item_idxs in train_dict.iteritems():
 				merged[user_idx] = train_dict[user_idx] + data_dict[user_idx]
 
+			print 'Adding negative samples to', len(data_tuples)
 			test_batch = add_negative_samples(data_tuples, merged, item_vecs.size()[0], num_negative)
+			print 'After adding', len(test_batch)
+			print test_batch[:20]
+			print 'Getting actual item vecs into place'
 			item_data, user_data, target = get_data_for_rcmdr(item_vecs, test_batch)
 
+			print 'Correct till here with total items/user', item_data.size(), user_data.size()
 			#Run forward pass to get the results
-			pred_target = rec_net(item_data, user_data)
-			loss = criterion(pred_target, target)
+			# pred_target = rec_net(item_data, user_data)
+			# loss = criterion(pred_target, target)
 
-			print 'Test time loss is', loss.data[0]
+			# print 'Test time loss is', loss.data[0]
 
-			compute_metrics(pred_target, test_batch)
+			# compute_metrics(pred_target, test_batch, topK=num_negative/10)
 			pass
 
 def run_recommender(batch_size=None, mode=None, num_epochs=None, num_negative=0, criterion=None, print_every = 100,checkpoint_name="Recommender_Network"):
@@ -221,4 +265,4 @@ def run_recommender(batch_size=None, mode=None, num_epochs=None, num_negative=0,
 
 if __name__ == '__main__':
 
-	run_recommender(batch_size=32, mode="train", num_epochs=10, num_negative=5, criterion=nn.MSELoss())
+	run_recommender(batch_size=32, mode="test", num_epochs=10, num_negative=5, criterion=nn.MSELoss())
